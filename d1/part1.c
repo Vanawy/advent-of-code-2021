ecee3ca2a6de1bb65b1bb84a576777c7f5760524
Ilya Sakharchuk <saharchuk999@gmail.com>
2021-12-02 17:47:55 +0300

    d1p1

---
+++ d1/part1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LEN 256
+
+int main (void)
+{
+    FILE *fp;
+    fp = fopen("d1/my.in", "r");
+
+    if (fp == NULL) {
+      perror("Failed");
+      return 1;
+    }
+
+    int prev = -1;
+    int count = 0;
+        
+    char buffer[MAX_LEN];
+    while (fgets(buffer, MAX_LEN, fp))
+    {
+        buffer[strcspn(buffer, "\n")] = 0;
+        int n = atoi(buffer);
+        if (prev == -1) {
+            prev = n;
+            continue;
+        }
+        if (n > prev) {
+            count++;
+        }
+        prev = n;
+
+    }
+    printf("%d\n", count);
+    fclose(fp);
+    return 0;
+}
